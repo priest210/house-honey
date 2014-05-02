1387e9f43d31bf0d10c15ab9134282fe227f51a7
Ken Priest <ken@priest210.com>
2014-05-02 10:32:06 -0500

    Initial set up for project.

---
+++ app/views/layouts/master.blade.php
@@ -0,0 +1,434 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>House Honey</title>
+
+    <!-- font-awesome -->
+    <link href="/bs-banzhow/css/font-awesome.min.css" rel="stylesheet">
+    <!--[if lt IE 9]>
+    <script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+    <!--[if IE 7]>
+    <link href="css/font-awesome-ie7.css" rel="stylesheet">
+    <![endif]-->
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <link href="/bs-banzhow/css/bootstrap.css" rel="stylesheet">
+
+    <!-- custom CSS -->
+    <link href="/bs-banzhow/css/style.css" rel="stylesheet">
+    <!--[if lte IE 8]>
+    <link href="css/ie8.css" rel="stylesheet">
+    <![endif]-->
+    <!--[if lte IE 7]>
+    <link href="css/ie7.css" rel="stylesheet">
+    <![endif]-->
+
+    <!-- JavaScript -->
+    <script type="text/javascript" src="/bs-banzhow/js/jquery-1.10.2.js"></script>
+    <script type="text/javascript" src="/bs-banzhow/js/bootstrap.js"></script>
+    <script type="text/javascript" src="/bs-banzhow/js/plugins.js"></script>
+    <!--[if lte IE 7]>
+    <script type="text/javascript" src="js/boxsizing/jquery.boxsizing.js"></script> 
+    <![endif]-->
+    <script type="text/javascript" src="/bs-banzhow/js/banzhow.js"></script>
+  </head>
+
+ 
+  <body>
+    
+
+    <nav class="navbar navbar-fixed-top navbar-inverse" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+
+          <a class="navbar-brand" href="/bs-banzhow/index.html">The House Honey</a>
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse navbar-ex1-collapse">
+          <ul class="nav navbar-nav navbar-right">
+            <li class="active"><a href="/bs-banzhow/index.html">Home</a></li>
+            <li><a href="/bs-banzhow/services.html">Services</a></li>
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown">Portfolio <i class="fa fa-angle-down"></i></a>
+              <ul class="dropdown-menu">
+                <li><a href="/bs-banzhow/portfolio-1-col.html">1 Column Portfolio</a></li>
+                <li><a href="/bs-banzhow/portfolio-2-col.html">2 Column Portfolio</a></li>
+                <li><a href="/bs-banzhow/portfolio-3-col.html">3 Column Portfolio</a></li>
+                <li><a href="/bs-banzhow/portfolio-item.html">Single Portfolio Item</a></li>
+              </ul>
+            </li>
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown">Blog <i class="fa fa-angle-down"></i></a>
+              <ul class="dropdown-menu">
+                <li><a href="/bs-banzhow/blog-home-1.html">Blog Home</a></li>
+                <li><a href="/bs-banzhow/blog-post.html">Blog Post</a></li>
+              </ul>
+            </li>
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown">Other Pages <i class="fa fa-angle-down"></i></a>
+              <ul class="dropdown-menu">
+                <li><a href="/bs-banzhow/full-width.html">Full Width Page</a></li>
+                <li><a href="/bs-banzhow/sidebar.html">Sidebar Page</a></li>
+                <li><a href="/bs-banzhow/faq.html">FAQ</a></li>
+                <li><a href="/bs-banzhow/404.html">404</a></li>
+              </ul>
+            </li>
+            <li><a href="/bs-banzhow/contact.html">Contact</a></li>
+          </ul>
+        </div><!-- /.navbar-collapse -->
+      </div><!-- /.container -->
+    </nav>
+
+
+    
+    <div id="myCarousel" class="carousel slide">
+      <!-- Indicators -->
+        <ol class="carousel-indicators">
+          <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+          <li data-target="#myCarousel" data-slide-to="1"></li>
+          <li data-target="#myCarousel" data-slide-to="2"></li>
+        </ol>
+
+        <!-- Wrapper for slides -->
+        <div class="carousel-inner">
+          <div class="item active">
+            <div class="fill" style="background-image:url('/bs-banzhow/img/slide/slide1.png');"></div>
+            <div class="carousel-caption">
+              <h2>House Honey Makes You Want to Rush Home!</h2>
+              <a href="#" class="button"></a>
+            </div>
+          </div>
+          <div class="item">
+            <div class="fill" style="background-image:url('/bs-banzhow/img/slide/slide2.png');"></div>
+            <div class="carousel-caption">
+              <h2>House Honey Makes You Smile When You Think of Her!</h2>
+              <a href="#" class="button">SEE MORE</a>
+            </div>
+          </div>
+          <div class="item">
+            <div class="fill" style="background-image:url('/bs-banzhow/img/slide/slide3.png');"></div>
+            <div class="carousel-caption">
+              <h2>House Honey is Every Guys Dream Girl!</h2>
+              <a href="#" class="button"></a>
+            </div>
+          </div>
+        </div>
+
+        <!-- Controls -->
+        <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+          <i class="fa fa-angle-left"></i>
+        </a>
+        <a class="right carousel-control" href="#myCarousel" data-slide="next">
+          <i class="fa fa-angle-right"></i>
+        </a>
+    </div>
+
+    <div class="section">
+
+      <div class="container">
+
+        <div class="row">
+          <div class="col-md-4 col-sm-4">
+            <div class="block-icon">
+              <i class="fa fa-glass"></i>
+            </div>
+
+            <div class="block-body">
+              <h2>Glasses</h2>
+              <div class="line-subtitle"></div>
+              <p>Wine glasses, beer mugs, Vino-to-Go and many more styles.</p>
+            </div>
+          </div>
+          <div class="col-md-4 col-sm-4">
+            <div class="block-icon">
+              <i class="glyphicon glyphicon t-shirt"></i>
+            </div>
+
+            <div class="block-body">
+              <h2>Shirts</h2>
+              <div class="line-subtitle"></div>
+              <p>I have a wide variety of shirt for you to choose from or you are more than welcome to provide your own.</p>
+            </div>
+          </div>
+          <div class="col-md-4 col-sm-4">
+            <div class="block-icon">
+              <i class="fa fa-scissors"></i>
+            </div>
+
+            <div class="block-body">
+              <h2>Customization</h2>
+              <div class="line-subtitle"></div>
+              <p>If you can think it up, I can make it up!</p>
+            </div>
+          </div>
+        </div><!-- /.row -->
+
+      </div><!-- /.container -->
+
+    </div><!-- /.section -->
+
+    <div class="section-colored home">
+
+      <div class="container">
+
+        <div class="row">
+          <div class="col-md-9 col-sm-8">
+            <h2>I will be happy to help you figure out a solution for your needs </h2>
+          </div>
+          <div class="col-md-3 col-sm-4">
+            <a href="#" class="btn btn-danger btn-lg">Contact me</a>
+          </div>
+        </div><!-- /.row -->
+
+      </div><!-- /.container -->
+
+    </div><!-- /.section-colored -->
+
+    <div class="section" id="recent-projects" style="position: relative;">
+
+      <div class="container">
+
+        <div class="row">
+          <div class="col-lg-12">
+            <h2 class="section-title">Recent Creations</h2>
+          </div>
+          <div class="col-lg-4 col-md-4 col-sm-6">
+            <a href="/bs-banzhow/portfolio-item.html" class="link-portfolio">
+              <div class="overlay-portfolio">
+                <h3>Banzhow Template</h3>
+                <small class="text-muted">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec pulvinar quam sed consequat adipiscing.
+                </small>
+              </div><!-- /.overlay-portfolio -->
+              <img class="img-responsive img-home-portfolio" src="/bs-banzhow/img/portfolio_1.png" alt="portfolio 1">
+            </a>
+          </div>
+          <div class="col-lg-4 col-md-4 col-sm-6">
+            <a href="/bs-banzhow/portfolio-item.html" class="link-portfolio">
+              <div class="overlay-portfolio">
+                <h3>The House Honey</h3>
+                <small class="text-muted">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec pulvinar quam sed consequat adipiscing.
+                </small>
+              </div><!-- /.overlay-portfolio -->
+              <img class="img-responsive img-home-portfolio" src="/bs-banzhow/img/portfolio_2.png" alt="portfolio 2">
+            </a>
+          </div>
+          <div class="col-lg-4 col-md-4 col-sm-6">
+            <a href="/bs-banzhow/portfolio-item.html" class="link-portfolio">
+              <div class="overlay-portfolio">
+                <h3>The House Honey</h3>
+                <small class="text-muted">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec pulvinar quam sed consequat adipiscing.
+                </small>
+              </div><!-- /.overlay-portfolio -->
+              <img class="img-responsive img-home-portfolio" src="/bs-banzhow/img/portfolio_3.png" alt="portfolio 3">
+            </a>
+          </div>
+          <div class="col-lg-4 col-md-4 col-sm-6">
+            <a href="/bs-banzhow/portfolio-item.html" class="link-portfolio">
+              <div class="overlay-portfolio">
+                <h3>The House Honey</h3>
+                <small class="text-muted">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec pulvinar quam sed consequat adipiscing.
+                </small>
+              </div><!-- /.overlay-portfolio -->
+              <img class="img-responsive img-home-portfolio" src="/bs-banzhow/img/portfolio_4.png" alt="portfolio 4">
+            </a>
+          </div>
+          <div class="col-lg-4 col-md-4 col-sm-6">
+            <a href="/bs-banzhow/portfolio-item.html" class="link-portfolio">
+              <div class="overlay-portfolio">
+                <h3>The House Honey</h3>
+                <small class="text-muted">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec pulvinar quam sed consequat adipiscing.
+                </small>
+              </div><!-- /.overlay-portfolio -->
+              <img class="img-responsive img-home-portfolio" src="/bs-banzhow/img/portfolio_5.png" alt="portfolio 5">
+            </a>
+          </div>
+          <div class="col-lg-4 col-md-4 col-sm-6">
+            <a href="/bs-banzhow/portfolio-item.html" class="link-portfolio">
+              <div class="overlay-portfolio">
+                <h3>The House Honey</h3>
+                <small class="text-muted">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec pulvinar quam sed consequat adipiscing.
+                </small>
+              </div><!-- /.overlay-portfolio -->
+              <img class="img-responsive img-home-portfolio" src="/bs-banzhow/img/portfolio_6.png" alt="portfolio 6">
+            </a>
+          </div>
+        </div><!-- /.row -->
+
+      </div><!-- /.container -->
+
+    </div><!-- /.section -->
+
+    <div class="section-colored">
+
+      <div class="container">
+        <h3 class="section-title text-center">Some of My Clients</h3>
+  
+        <div class="container clients">
+
+          <div class="row">
+            <div class="col-lg-2 col-md-2 col-sm-2 col-xs-6">
+              <a href="#"><img class="img-responsive" src="/bs-banzhow/img/freedom-cup-smaller.png" alt="Freedom Cup"></a>
+            </div>
+
+            <div class="col-lg-2 col-md-2 col-sm-2 col-xs-6">
+              <a href="#"><img class="img-responsive" src="/bs-banzhow/img/" alt="client 2"></a>
+            </div>
+            
+            <div class="col-lg-2 col-md-2 col-sm-2 col-xs-6">
+              <a href="#"><img class="img-responsive" src="/bs-banzhow/img/" alt="client 3"></a>
+            </div>
+
+            <div class="col-lg-2 col-md-2 col-sm-2 col-xs-6">
+              <a href="#"><img class="img-responsive" src="/bs-banzhow/vimg/" alt="client 4"></a>
+            </div>
+
+            <div class="col-lg-2 col-md-2 col-sm-2 col-xs-6">
+              <a href="#"><img class="img-responsive" src="/bs-banzhow/img/" alt="client 5"></a>
+            </div>
+
+            <div class="col-lg-2 col-md-2 col-sm-2 col-xs-6">
+              <a href="#"><img class="img-responsive" src="/bs-banzhow/img/" alt="client 6"></a>
+            </div>
+          </div><!-- /.row -->
+
+        </div><!-- /.container -->
+
+      </div><!-- /.container -->
+
+    </div><!-- /.section-colored -->
+
+    <footer>
+      <div class="container">
+
+        <div class="row">
+          <div class="col-lg-3 col-md-3">
+            <a class="footer-brand" href="index.html">The House Honey</a>
+            <p>She's that girl that makes you want to rush home.  She is that thought that gets you tickled and makes you smile.  She is that touch that calms you.  She is everything good in this world.</p>
+            <ul class="list-inline list-unstyled social-networks">
+              <li>
+                <a href="http://www.facebook.com/househoneydesigns">
+                  <div class="icon-social icon-social-facebook normal">
+                    <i class="fa fa-facebook"></i>
+                  </div>
+                  <div class="icon-social hover">
+                    <i class="fa fa-facebook"></i>
+                  </div>
+                </a>
+              </li>
+
+              <li>
+                <a href="@House_Honey">
+                  <div class="icon-social icon-social-twitter normal">
+                    <i class="fa fa-twitter"></i>
+                  </div>
+                  <div class="icon-social hover">
+                    <i class="fa fa-twitter"></i>
+                  </div>
+                </a>
+              </li>
+
+              <li>
+                <a href="#">
+                  <div class="icon-social icon-social-google-plus normal">
+                    <i class="fa fa-google-plus"></i>
+                  </div>
+                  <div class="icon-social hover">
+                    <i class="fa fa-google-plus"></i>
+                  </div>
+                </a>
+              </li>
+
+              <li>
+                <a href="#">
+                  <div class="icon-social icon-social-linkedin normal">
+                    <i class="fa fa-linkedin"></i>
+                  </div>
+                  <div class="icon-social hover">
+                    <i class="fa fa-linkedin"></i>
+                  </div>
+                </a>
+              </li>
+
+              <li>
+                <a href="http://www.pinterest.com/House_Honey/">
+                  <div class="icon-social icon-social-pinterest normal">
+                    <i class="fa fa-pinterest"></i>
+                  </div>
+                  <div class="icon-social hover">
+                    <i class="fa fa-pinterest"></i>
+                  </div>
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div class="col-lg-3 col-md-3">
+            <h3>CONTACT</h3>
+            <address>
+              <i class="fa fa-map-marker"></i> San Antonio, TX 78253<br>
+              <i class="fa fa-phone"></i> (210) 454-2139<br>
+              <i class="fa fa-envelope"></i> heather@househoney.com
+            </address>
+          </div>
+          
+          <div class="col-lg-3 col-md-3">
+            <h3>LATEST TWEETS</h3>
+            <ul class="latest-tweets">
+              <li>
+                <i class="fa fa-twitter"></i> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec pulvinar quam sed consequat adipiscing
+              </li>
+              <li>
+                <i class="fa fa-twitter"></i> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec pulvinar quam sed consequat adipiscing
+              </li>
+            </ul>
+          </div>
+
+          <div class="col-lg-3 col-md-3">
+            <h3>NEWSLETTER</h3>
+            <p>Type your email address below and subscribe to our newsletter</p>
+            <div class="input-group">
+              <input type="text" placeholder="Email" class="form-control">
+              <span class="input-group-btn">
+                <button class="btn btn-primary" type="button">Go!</button>
+              </span>
+            </div><!-- /input-group -->
+          </div>
+        </div><!-- /.row -->
+
+      </div><!-- /.container -->
+    </footer>
+
+    <div class="footer-after">
+      <div class="container">
+        <div class="row">
+          <p class="col-md-10">©2014 All rights reserved. <a href="kenpriest.me" alt="kenpriest.me">Designed by Ken Priest - kenpriest.me</a></p>
+          <p class="col-md-2 legal"><a href="#" rel="nofollow">Legal Notice</a></p>
+        </div><!-- /.row -->
+      </div>
+    </div>
+    
+    
+
+    
+
+  </body>
+</html>
